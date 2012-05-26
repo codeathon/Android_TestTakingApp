a3cfa8bf70879a55607523710e11b64d214d5380
codeathon <rohit.ainapure@gmail.com>
2012-05-26 02:00:03 -0400

    UI modified

--- gen/com/android/test/R.java
+++ gen/com/android/test/R.java
@@ -11,36 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int viewer=0x7f020001;
+        public static final int bb=0x7f020000;
+        public static final int bb2=0x7f020001;
+        public static final int bg=0x7f020002;
+        public static final int border=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int viewer=0x7f020005;
     }
     public static final class id {
-        public static final int Choice1=0x7f050010;
-        public static final int Choice2=0x7f050011;
-        public static final int Choice3=0x7f050012;
-        public static final int Choice4=0x7f050013;
-        public static final int Choices=0x7f05000f;
-        public static final int Confirm=0x7f050017;
-        public static final int Next=0x7f050015;
-        public static final int Prev=0x7f050014;
-        public static final int Question=0x7f05000e;
-        public static final int Submit=0x7f050016;
-        public static final int a_Choice1=0x7f050003;
-        public static final int a_Choice2=0x7f050004;
-        public static final int a_Choice3=0x7f050005;
-        public static final int a_Choice4=0x7f050006;
-        public static final int a_Choices=0x7f050002;
-        public static final int a_Next=0x7f050008;
-        public static final int a_Prev=0x7f050007;
-        public static final int a_Question=0x7f050001;
-        public static final int cnum=0x7f050018;
-        public static final int editText1=0x7f050009;
-        public static final int editText2=0x7f05000a;
-        public static final int exit=0x7f05000b;
-        public static final int lay=0x7f050000;
-        public static final int q_no=0x7f050019;
-        public static final int vans=0x7f05000c;
-        public static final int widget27=0x7f05000d;
+        public static final int Choice1=0x7f060010;
+        public static final int Choice2=0x7f060011;
+        public static final int Choice3=0x7f060012;
+        public static final int Choice4=0x7f060013;
+        public static final int Choices=0x7f06000f;
+        public static final int Confirm=0x7f060017;
+        public static final int Next=0x7f060014;
+        public static final int Prev=0x7f060018;
+        public static final int Question=0x7f06000e;
+        public static final int Submit=0x7f060019;
+        public static final int a_Choice1=0x7f060003;
+        public static final int a_Choice2=0x7f060004;
+        public static final int a_Choice3=0x7f060005;
+        public static final int a_Choice4=0x7f060006;
+        public static final int a_Choices=0x7f060002;
+        public static final int a_Next=0x7f060008;
+        public static final int a_Prev=0x7f060007;
+        public static final int a_Question=0x7f060001;
+        public static final int cnum=0x7f060015;
+        public static final int editText1=0x7f060009;
+        public static final int editText2=0x7f06000a;
+        public static final int exit=0x7f06000b;
+        public static final int lay=0x7f060000;
+        public static final int q_no=0x7f060016;
+        public static final int vans=0x7f06000c;
+        public static final int widget27=0x7f06000d;
     }
     public static final class layout {
         public static final int answers=0x7f030000;
@@ -49,7 +53,12 @@ public final class R {
         public static final int test=0x7f030003;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+    }
+    public static final class style {
+        public static final int button=0x7f040000;
+        public static final int choice=0x7f040001;
+        public static final int question=0x7f040002;
     }
 }
